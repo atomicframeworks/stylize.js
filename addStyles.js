a76c856089856e298f5406f1745a1e7991ddf1f7
Kevin McGinty <atomic.frameworks@gmail.com>
2014-01-03 21:49:27 -0600

    Remove jQuery dependency

--- addStyles.js
+++ addStyles.js
@@ -1,17 +1,17 @@
-/*
+/*!
                         
        ##    #####      Copyright (c) - Kevin McGinty
      # _ #  ###        
     #   #  #            AtomicFrameworks
     
 */
-/*global $*/
+/*global document*/
 
-// Add a new style block to the head containing the selectors and their associated declarations
+// Add a new style block to the head ontaining the selectors and their associated declarations
 function addStyles(styleObj) {
     "use strict";
     // Init vars
-    var styleString = '', selector, declarationBlock, declaration;
+    var styleString = '', selector, declarationBlock, declaration, styleElement;
     // Loop styleObj to create string
     for (selector in styleObj) {
         if (styleObj.hasOwnProperty(selector)) {
@@ -21,13 +21,22 @@ function addStyles(styleObj) {
             // Loop through each declaration in the block and add it
             for (declaration in declarationBlock) {
                 if (declarationBlock.hasOwnProperty(declaration)) {
-                    styleString += declaration + ': ' + declarationBlock[declaration] + ';';
+                    styleString += declaration + ': ' + declarationBlock[declaration].replace(/;/g, '') + ';';
                 }
             }
             // Close the declaration block
             styleString += '}';
         }
     }
-    // Add the styles to the head
-    $('<style type="text/css">' + styleString + '</style>').appendTo('head');
+
+    // Create new element with the styles and add it to head
+    styleElement = document.createElement('style');
+    styleElement.type = 'text/css';
+    if (styleElement.styleSheet) {
+        // IE
+        styleElement.styleSheet.cssText = styleString;
+    } else {
+        styleElement.appendChild(document.createTextNode(styleString));
+    }
+    document.getElementsByTagName('head')[0].appendChild(styleElement);
 }
